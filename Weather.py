94b8d14017acac48ade094a281ed21e9137cf3aa
Ishan <hiraniishan17@gmail.com>
2026-02-27 00:15:42 +0530

    Created weatherchatbot

---
+++ Weather.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+import requests
+import os
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+print(API_KEY)
+
+def get_weather(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if data["cod"] != 200:
+        print("City not found!")
+        return
+
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    condition = data["weather"][0]["description"]
+
+    print(f"\nWeather in {city}:")
+    print(f"Temperature: {temp}°C")
+    print(f"Humidity: {humidity}%")
+    print(f"Condition: {condition}")
+
+def main():
+    while True:
+        city = input("\nEnter city name (or 'exit'): ")
+
+        if city.lower() == "exit":
+            break
+
+        get_weather(city)
+
+if __name__ == "__main__":
+    main()
